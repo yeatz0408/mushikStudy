4c9605199e554b6889be6ed766f45326f80d1218
jungn <jung.noah@rsp-corp.com>
2024-07-01 11:59:54 +0900

    feat: wrap the object in ResponseEntity

--- src/main/java/mushikStudy/com/mushikStudy/controllers/KanjiController.java
+++ src/main/java/mushikStudy/com/mushikStudy/controllers/KanjiController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import mushikStudy.com.mushikStudy.constants.ApiV1;
 import mushikStudy.com.mushikStudy.dto.response.KanjiResponse;
 import mushikStudy.com.mushikStudy.services.KanjiService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class KanjiController {
     private final KanjiService service;
 
     @GetMapping(ApiV1.LOAD_KANJI)
-    public KanjiResponse loadKanji(@RequestParam(name="pageNo") long pageNo, @RequestParam(name="pageSize") int pageSize) {
-        return service.load(pageNo, pageSize);
+    public ResponseEntity<KanjiResponse> loadKanji(@RequestParam(name="pageNo") long pageNo, @RequestParam(name="pageSize") int pageSize) {
+        return ResponseEntity.ok(service.load(pageNo, pageSize));
     }
 }
