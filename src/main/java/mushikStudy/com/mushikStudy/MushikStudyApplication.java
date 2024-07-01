382b135b12d60830fcf6f7db8fde63c7eaae0d00
jungn <jung.noah@rsp-corp.com>
2024-07-01 09:27:46 +0900

    start

---
+++ src/main/java/mushikStudy/com/mushikStudy/MushikStudyApplication.java
@@ -0,0 +1,13 @@
+package mushikStudy.com.mushikStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MushikStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MushikStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mushikStudy/com/mushikStudy/MushikStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package mushikStudy.com.mushikStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MushikStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
