2755662453981e5478fdad6e76e983be8f310272
jungn <jung.noah@rsp-corp.com>
2024-07-01 12:50:20 +0900

    feat: enable log colorings

--- src/main/java/mushikStudy/com/mushikStudy/MushikStudyApplication.java
+++ src/main/java/mushikStudy/com/mushikStudy/MushikStudyApplication.java
@@ -1,13 +1,21 @@
 package mushikStudy.com.mushikStudy;
 
+import jakarta.annotation.PostConstruct;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @SpringBootApplication
 public class MushikStudyApplication {
 
+	private static final Logger logger = LogManager.getLogger(MushikStudyApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(MushikStudyApplication.class, args);
 	}
-
 }
