81e59e7a0012c8ff07687203f66b117938f6521c
jungn <jung.noah@rsp-corp.com>
2024-07-02 12:23:05 +0900

    feat: add more api urls

--- src/main/java/mushikStudy/com/mushikStudy/constants/ApiV1.java
+++ src/main/java/mushikStudy/com/mushikStudy/constants/ApiV1.java
@@ -3,7 +3,16 @@ package mushikStudy.com.mushikStudy.constants;
 public class ApiV1 {
     private static final String VER = "/v1";
 
-    public static final String LOAD_KANJI = VER + "/load/kanji";
+    public static final String LOAD_KANJI = VER + "/jp/kanji/load";
+    public static final String LOAD_KANJI_RANDOM = VER + "/jp/kanji/load/random";
+
+    public static final String ADD_KANJI = VER + "/jp/kanji/add";
+    public static final String ADD_KANJI_PUSH = VER + "/jp/kanji/push";
+    public static final String ADD_KANJI_REAR = VER + "/jp/kanji/rear";
+
+    public static final String DELETE_KANJI = VER + "/jp/kanji/delete";
+    public static final String DELETE_KANJI_FRONT = VER + "/jp/kanji/delete/front";
+    public static final String DELETE_KANJI_REAR = VER + "/jp/kanji/delete/rear";
 
     private ApiV1() {
     }
